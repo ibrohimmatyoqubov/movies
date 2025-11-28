e05d9168d4020afe16455ea06be472f44c89b4ab
ibrohimmatyoqubov <ibush0925@gmail.com>
2025-11-28 15:31:17 +0500

    transfer function comonent and change useMovieService hook

--- src/components/hero/hero.jsx
+++ src/components/hero/hero.jsx
@@ -1,70 +1,54 @@
 import PropTypes from 'prop-types'
-import React from 'react'
-import MovieService from '../../services/movie-service'
+import React, { useEffect, useState } from 'react'
+import useMovieService from '../../services/movie-service'
 import Error from '../error/error'
 import Spinner from '../spinner/spinner'
 import './hero.scss'
 
-class Hero extends React.Component {
-	constructor(props) {
-		super(props)
-		this.state = {
-			movie: {},
-			loading: true,
-			error: false,
-		}
+const Hero = () => {
+	const [movie, setMovie] = useState({})
 
-		this.movieService = new MovieService()
+	const { getRandomMovie, loading, error, clearError } = useMovieService()
+
+	useEffect(() => {
+		updateMovie()
+	}, [])
+
+	const updateMovie = () => {
+		clearError()
+		getRandomMovie().then(res => setMovie(res))
 	}
 
-	componentDidMount() {
-		this.updateMovie()
-	}
+	const errorContent = error ? <Error /> : null
+	const loadingContent = loading ? <Spinner /> : null
+	const content = !(error || loading || !movie) ? (
+		<Content movie={movie} updateMovie={updateMovie} />
+	) : null
 
-	updateMovie = () => {
-		this.setState({ loading: true })
-
-		this.movieService
-			.getRandomMovie()
-			.then(res => this.setState({ movie: res }))
-			.catch(() => this.setState({ error: true }))
-			.finally(() => this.setState({ loading: false }))
-	}
-
-	render() {
-		const { movie, loading, error } = this.state
-
-		const errorContent = error ? <Error /> : null
-		const loadingContent = loading ? <Spinner /> : null
-		const content = !(error || loading) ? (
-			<Content movie={movie} updateMovie={this.updateMovie} />
-		) : null
-
-		return (
-			<div className='app__hero'>
-				<div className='app__hero-info'>
-					<h2>FIND MOVIES</h2>
-					<h1>TV shows and more</h1>
-					<p>
-						Lorem ipsum dolor sit amet consectetur adipisicing elit. Illum
-						sapiente sit placeat minus dolorum, magnam, tempora quas neque
-						quasi, sequi odit doloremque velit saepe autem facilis! Laudantium
-						consequatur accusantium mollitia.
-					</p>
-					<div>
-						<button className='btn btn__secondary' onClick={this.updateMovie}>
-							RANDOM MOVIE
-						</button>
-					</div>
-				</div>
-				<div className='app__hero-moive'>
-					{errorContent}
-					{loadingContent}
-					{content}
+	return (
+		<div className='app__hero'>
+			<div className='app__hero-info'>
+				<h2>FIND MOVIES</h2>
+				<h1>TV shows and more</h1>
+				<p>
+					Lorem ipsum dolor sit amet consectetur adipisicing elit. Illum
+					sapiente sit placeat minus dolorum, magnam, tempora quas neque quasi,
+					sequi odit doloremque velit saepe autem facilis! Laudantium
+					consequatur accusantium mollitia.
+				</p>
+				<div>
+					<button className='btn btn__secondary' onClick={updateMovie}>
+						RANDOM MOVIE
+					</button>
 				</div>
 			</div>
-		)
-	}
+			<div className='app__hero-moive'>
+				{errorContent}
+				{loadingContent}
+				{content}
+			</div>
+		</div>
+	)
 }
 
 export default Hero
