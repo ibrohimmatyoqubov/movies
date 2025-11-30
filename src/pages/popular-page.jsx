3f1af1fcca177a4992e3b75bcc5e96471d8d8954
ibrohimmatyoqubov <ibush0925@gmail.com>
2025-11-30 17:12:26 +0500

    add new pages:detailed, home, not-found, trending, popular

---
+++ src/pages/popular-page.jsx
@@ -0,0 +1,8 @@
+import React from 'react'
+import RowMovies from '../components/row-movies/row-movies'
+
+const PopularPage = () => {
+	return <RowMovies />
+}
+
+export default PopularPage
---
+++ src/pages/trending-page.jsx
@@ -0,0 +1,7 @@
+import RowMovies from '../components/row-movies/row-movies'
+
+const TrendingPage = () => {
+	return <RowMovies />
+}
+
+export default TrendingPage
