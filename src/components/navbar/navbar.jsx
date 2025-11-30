66e57f7a899d4c156bd92cac94cc9134f0d85940
ibrohimmatyoqubov <ibush0925@gmail.com>
2025-11-30 17:10:44 +0500

    add navbar component

---
+++ src/components/navbar/navbar.jsx
@@ -0,0 +1,35 @@
+import { Link, NavLink } from 'react-router-dom'
+import { navbar_links } from '../../constants'
+import './navbar.scss'
+import logoText from '/logo-text.svg'
+import logo from '/logo.svg'
+
+const Navbar = () => {
+	return (
+		<div className='navbar'>
+			<div className='navbar__logo'>
+				<Link to={'/'}>
+					<img className='navbar__logo-icon' src={logo} alt='Logo' />
+					<img className='navbar__logo-text' src={logoText} alt='Logo Text' />
+				</Link>
+			</div>
+
+			<nav className='navbar__menu'>
+				<ul>
+					{navbar_links.map(item => (
+						<li key={item.route}>
+							<NavLink
+								to={item.route}
+								className={({ isActive }) => (isActive ? 'active' : '')}
+							>
+								{item.label}
+							</NavLink>
+						</li>
+					))}
+				</ul>
+			</nav>
+		</div>
+	)
+}
+
+export default Navbar
