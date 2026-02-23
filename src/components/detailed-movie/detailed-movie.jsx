ea36d7efe5bbc9b6bc541a2a63c02a32abf20bda
ibrohimmatyoqubov <ibush0925@gmail.com>
2026-02-23 13:25:52 +0500

    add MovieTrailer component and bugs fix

--- src/components/detailed-movie/detailed-movie.jsx
+++ src/components/detailed-movie/detailed-movie.jsx
@@ -1,12 +1,16 @@
 import PropTypes from 'prop-types'
 import { useEffect, useState } from 'react'
+import Modal from 'react-responsive-modal'
 import { useParams } from 'react-router-dom'
 import useMovieService from '../../services/movie-service'
+import Error from '../error/error'
+import MovieTrailer from '../movie-trailer/movie-trailer'
 import Spinner from '../spinner/spinner'
 import './detailed-movie.scss'
 
 const DetailedMovie = () => {
 	const [movie, setMovie] = useState(null)
+	const [isModalOpen, setIsModalOpen] = useState(false)
 
 	const { movieId } = useParams()
 
@@ -24,10 +28,18 @@ const DetailedMovie = () => {
 		getDetailedMovie(movieId).then(res => setMovie(res))
 	}
 
+	const openTrailerModal = () => {
+		setIsModalOpen(true)
+	}
+
+	const closeModal = () => {
+		setIsModalOpen(false)
+	}
+
 	const errorContent = error ? <Error /> : null
 	const loadingContent = loading ? <Spinner /> : null
 	const content = !(error || loading || !movie) ? (
-		<Content movie={movie} />
+		<Content movie={movie} onWatchTrailer={openTrailerModal} />
 	) : null
 
 	return (
@@ -35,13 +47,51 @@ const DetailedMovie = () => {
 			{errorContent}
 			{loadingContent}
 			{content}
+
+			<Modal
+				open={isModalOpen}
+				onClose={closeModal}
+				center
+				styles={{
+					overlay: {
+						display: 'flex',
+						flexDirection: 'column',
+						alignItems: 'center',
+						justifyContent: 'center',
+					},
+					modalContainer: {
+						display: 'flex',
+						alignItems: 'center',
+						justifyContent: 'center',
+						height: '100%',
+					},
+					modal: {
+						padding: '1.5rem',
+						width: 'min(1000px, 92vw)',
+						maxHeight: '90vh',
+						overflow: 'auto',
+						borderRadius: '12px',
+						overflowX: 'hidden',
+					},
+					closeButton: {
+						top: '5px',
+						right: '5px',
+						width: '25px',
+						height: '25px',
+						borderRadius: '50%',
+						boxShadow: '0 0 5px rgba(0,0,0,0.2)',
+					},
+				}}
+			>
+				{isModalOpen && <MovieTrailer movieId={movieId} />}
+			</Modal>
 		</>
 	)
 }
 
 export default DetailedMovie
 
-const Content = ({ movie }) => {
+const Content = ({ movie, onWatchTrailer }) => {
 	return (
 		<div className='detailedmovie'>
 			<div className='detailedmovie__image'>
@@ -57,7 +107,9 @@ const Content = ({ movie }) => {
 					<p>{movie.vote_average.toFixed(1)}</p>
 					<img src='/star.svg' alt='star' />
 				</div>
-				<button className='btn btn__secondary'>Watch Trailer</button>
+				<button className='btn btn__secondary' onClick={onWatchTrailer}>
+					Watch Trailer
+				</button>
 			</div>
 		</div>
 	)
@@ -65,4 +117,5 @@ const Content = ({ movie }) => {
 
 Content.propTypes = {
 	movie: PropTypes.object,
+	onWatchTrailer: PropTypes.func.isRequired,
 }
